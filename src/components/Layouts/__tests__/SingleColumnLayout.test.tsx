efb20c5ed99ea3d18d3325b4c3a2339567bd061f
Pham Quan <6479147+mikunpham@users.noreply.github.com>
2022-06-28 13:00:25 +0700

    feat: add react testing-library (#6)

---
+++ src/components/Layouts/__tests__/SingleColumnLayout.test.tsx
@@ -0,0 +1,18 @@
+import { render, screen } from '@testing-library/react';
+import { SingleColumnLayout } from '../index';
+
+describe('SingleColumnLayout', () => {
+  test('Should render SingleColumnLayout without crashing', () => {
+    render(
+      <SingleColumnLayout
+        renderHeader={<div>Header</div>}
+        renderMain={<div>Main</div>}
+        renderFooter={<div>Footer</div>}
+      />
+    );
+
+    expect(screen.getByRole('banner')).toHaveTextContent('Header');
+    expect(screen.getByRole('main')).toHaveTextContent('Main');
+    expect(screen.getByRole('contentinfo')).toHaveTextContent('Footer');
+  });
+});
