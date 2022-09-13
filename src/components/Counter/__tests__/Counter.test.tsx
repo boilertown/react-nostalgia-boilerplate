55d3605aced534df2d52efb70d2ce20e444a2c68
Quan Pham <6479147+mikunpham@users.noreply.github.com>
2022-09-13 12:28:37 +0700

    chore: update packages version

--- src/components/Counter/__tests__/Counter.test.tsx
+++ src/components/Counter/__tests__/Counter.test.tsx
@@ -1,14 +1,17 @@
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Counter } from '../index';
 
 describe('Counter', () => {
 	test('Should render correct counter', async () => {
 		const user = userEvent.setup();
+
 		render(<Counter />);
+
 		const counterButton = screen.getByRole('button');
 		expect(counterButton).toHaveTextContent('Count: 0');
-		await user.click(screen.getByRole('button'));
+
+		await act(() => user.click(screen.getByRole('button')));
 		expect(counterButton).toHaveTextContent('Count: 1');
 	});
 });
